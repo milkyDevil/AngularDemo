1781e903257c41718746c28573c38b99c3fbe2d3
Mahmuda Begum <begum.mahmuda16@gmail.com>
2024-01-04 22:47:45 +0530

    recipe detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { GetstartedComponent } from './pages/getstarted/getstarted.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RecipeComponent } from './pages/recipe/recipe/recipe.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FooterComponent } from './components/footer/footer.component';
     LoginComponent,
     GetstartedComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
