5c4dfe8d2dbbd4304d0a76622ae0cc2b358844c2
Mahmuda Begum <begum.mahmuda16@gmail.com>
2024-01-06 22:50:53 +0530

    login register ui done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GetstartedComponent } from './pages/getstarted/getstarted.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RecipeComponent } from './pages/recipe/recipe.component';
+import { LoginregisterComponent } from './pages/loginregister/loginregister.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
     GetstartedComponent,
     HeaderComponent,
     FooterComponent,
-    RecipeComponent
+    RecipeComponent,
+    LoginregisterComponent
   ],
   imports: [
     BrowserModule,
