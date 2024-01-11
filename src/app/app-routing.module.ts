81a793c560318606894f598e7b924c4510d464c1
Mahmuda Begum <begum.mahmuda16@gmail.com>
2024-01-11 12:57:38 +0530

    routing angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetstartedComponent } from './pages/getstarted/getstarted.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginregisterComponent } from './pages/loginregister/loginregister.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { UploadrecipeComponent } from './pages/uploadrecipe/uploadrecipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'getstared', component: GetstartedComponent },
+  { path: 'loginregister', component: LoginregisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'recipe', component: RecipeComponent },
+  { path: 'uploadrecipe', component: UploadrecipeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
